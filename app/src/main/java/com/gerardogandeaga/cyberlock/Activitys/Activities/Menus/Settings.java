3768a2256a6a8fcce08378ba9dc0ea3c527a5b2a
gerardogandeaga <gerardogandeaga@Gmail.com>
2017-08-22 13:57:59 -0700

    Unfinished LogoutDelay Timer

--- app/src/main/java/com/gerardogandeaga/cyberlock/Activitys/Activities/Menus/Settings.java
+++ app/src/main/java/com/gerardogandeaga/cyberlock/Activitys/Activities/Menus/Settings.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -30,7 +32,8 @@ public class Settings extends AppCompatActivity implements View.OnClickListener
 {
     // DATA
     private SharedPreferences mSharedPreferences;
-    private static final String KEY = "KEY", PIN = "PIN", AUTOSAVE = "AUTOSAVE";
+    public static final String DIRECTORY = "com.gerardogandeaga.cyberlock";
+    private static final String KEY = "KEY", PIN = "PIN", AUTOSAVE = "AUTOSAVE", DELAY = "DELAY";
     private static final int flags = Base64.DEFAULT;
     private String mAutoLogoutDelay;
 
@@ -84,7 +87,23 @@ public class Settings extends AppCompatActivity implements View.OnClickListener
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
             {
                 Object object = parent.getItemAtPosition(position);
-                if (object != null) { mAutoLogoutDelay = object.toString(); }
+                if (object != null)
+                {
+                    mAutoLogoutDelay = object.toString();
+                    switch (mAutoLogoutDelay)
+                    {
+                        case "Immediate": getSharedPreferences(DIRECTORY, Context.MODE_PRIVATE).edit().putInt(DELAY, 0);
+                        case "15 Seconds": getSharedPreferences(DIRECTORY, Context.MODE_PRIVATE).edit().putInt(DELAY, 15000);
+                        case "30 Seconds": getSharedPreferences(DIRECTORY, Context.MODE_PRIVATE).edit().putInt(DELAY, 30000);
+                        case "1 Minute": getSharedPreferences(DIRECTORY, Context.MODE_PRIVATE).edit().putInt(DELAY, 60000);
+                        case "5 Minutes": getSharedPreferences(DIRECTORY, Context.MODE_PRIVATE).edit().putInt(DELAY, 300000);
+                        case "10 Minutes": getSharedPreferences(DIRECTORY, Context.MODE_PRIVATE).edit().putInt(DELAY, 600000);
+                        case "30 Minutes": getSharedPreferences(DIRECTORY, Context.MODE_PRIVATE).edit().putInt(DELAY, 1800000);
+                        case "1 Hour": getSharedPreferences(DIRECTORY, Context.MODE_PRIVATE).edit().putInt(DELAY, 3600000);
+                        case "2 Hours": getSharedPreferences(DIRECTORY, Context.MODE_PRIVATE).edit().putInt(DELAY, 7200000);
+                        case "Never": ;
+                    }
+                }
             }
 
             @Override
@@ -214,7 +233,20 @@ public class Settings extends AppCompatActivity implements View.OnClickListener
         {
             if (ACTIVITY_INTENT == null) // NO PENDING ACTIVITIES ???(MAIN)--->(EDIT)???
             {
-                new LogoutProtocol().logoutExecuteAutosaveOff(this);
+                new AsyncTask<Void,Void, Void>()
+                {
+                    @Override
+                    protected Void doInBackground(Void... params)
+                    {
+                        new CountDownTimer(getSharedPreferences(DIRECTORY, Context.MODE_PRIVATE).getInt(DELAY, 0), 1000)
+                        {
+
+                        }
+
+                        new LogoutProtocol().logoutExecuteAutosaveOff(mContext);
+                        return null;
+                    }
+                }.execute();
             }
         }
     }
